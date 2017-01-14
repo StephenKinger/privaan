b7471dd40940dbb6fcc3a0ece62568df90863e9c
Keezy <stephen.kinger@gmail.com>
2017-01-14 13:43:27 +0100

    Add request information, travis ci and licence informations in readme

--- privaan/apacheLogListener.py
+++ privaan/apacheLogListener.py
@@ -15,34 +15,37 @@ class AccessEntry():
         self.request = record[5] + record[6] + record[7]
         self.status_code = record[8]
         self.size = record[9]
-        
+
     def isGranted(self):
         return self.status_code == '200'
-        
+
     def isUnauthorized(self):
         return self.status_code == '401'
-        
+
     def isBadRequest(self):
         return self.status_code == '400'
-        
+
     def getIp(self):
         return self.ip
-        
+
     def getDate(self):
         return datetime.strptime(self.date, "[%d/%b/%Y:%H:%M:%S")
 
+    def getRequest(self):
+        return self.request
+
 class ApacheLogListener():
-    
+
     def __init__(self, logfile):
         self.logfile=logfile
-        
+
     def Watch(self, callback):
         self.first_run = 1
         while 1:
             self.Check(callback)
             time.sleep(15)
             print "relances"
-        
+
     def Check(self, callback):
         if self.first_run:
             self.startdate = datetime.now()
@@ -66,6 +69,7 @@ class ApacheLogListener():
                 if access_entry.isUnauthorized():
                     access_type = 'UNAUTHORIZED'
                 msg += '<p>'+access_type + ' access occured from IP ' + access_entry.getIp() + '</p>\n'
+                msg += '<p>Request '+access_entry.getRequest()+'</p>\n'
                 geo = GeoIPInformation()
                 msg += geo.GetIPInfo(access_entry.getIp())
             else:
@@ -75,5 +79,5 @@ class ApacheLogListener():
           </body>
         </html>
         """
-        if is_something_to_send:    
+        if is_something_to_send:
             callback(msg)
