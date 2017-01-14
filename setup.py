5f58a847a9ef783ecfa6b583b59fb3fd55a35726
Keezy <stephen.kinger@gmail.com>
2017-01-14 14:41:56 +0100

    requests dependancy forgotten

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 import os
- 
+
 # notez qu'on import la lib
 # donc assurez-vous que l'importe n'a pas d'effet de bord
 import privaan
@@ -29,7 +29,7 @@ class PostInstallCommand(install):
         user_max_box_api_key = raw_input("Enter the map box api key (https://www.mapbox.com): ")
         os.system("rm privaan/config.py && touch privaan/config.py")
         f = open('privaan/config.py', 'w')
-        
+
         f.write('username = \''+username+'\'\n')
         f.write('password = \''+password+'\'\n')
         f.write('fromaddr = \''+sender_email+'\'\n')
@@ -45,13 +45,13 @@ class PostInstallCommand(install):
 # Ceci n'est qu'un appel de fonction. Mais il est trèèèèèèèèèèès long
 # et il comporte beaucoup de paramètres
 setup(
- 
+
     # le nom de votre bibliothèque, tel qu'il apparaitre sur pypi
     name='privaan',
- 
+
     # la version du code
     version=privaan.__version__,
- 
+
     # Liste les packages à insérer dans la distribution
     # plutôt que de le faire à la main, on utilise la foncton
     # find_packages() de setuptools qui va cherche tous les packages
@@ -59,21 +59,21 @@ setup(
     # C'est pour cette raison que l'on a tout mis dans un seul dossier:
     # on peut ainsi utiliser cette fonction facilement
     packages=find_packages(),
- 
+
     # votre pti nom
     author="Stephen KINGER",
- 
+
     # Votre email, sachant qu'il sera publique visible, avec tous les risques
     # que ça implique.
     author_email="",
- 
+
     # Une description courte
     description="Tool to monitor apache logs and notify on connexions",
- 
+
     # Une description longue, sera affichée pour présenter la lib
     # Généralement on dump le README ici
     long_description=open('README.md').read(),
- 
+
     # Vous pouvez rajouter une liste de dépendances pour votre lib
     # et même préciser une version. A l'installation, Python essayera de
     # les télécharger et les installer.
@@ -83,13 +83,13 @@ setup(
     # Dans notre cas on en a pas besoin, donc je le commente, mais je le
     # laisse pour que vous sachiez que ça existe car c'est très utile.
     # install_requires= ,
- 
+
     # Active la prise en compte du fichier MANIFEST.in
     include_package_data=True,
- 
+
     # Une url qui pointe vers la page officielle de votre lib
     url='http://github.com/StephenKinger/privaan',
- 
+
     # Il est d'usage de mettre quelques metadata à propos de sa lib
     # Pour que les robots puissent facilement la classer.
     # La liste des marqueurs autorisées est longue:
@@ -106,37 +106,38 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Topic :: Security",
     ],
- 
+
     install_requires=[
         'mock',
         'pygtail',
         'docopt==0.6.2',
+        'requests,'
     ],
-    
+
     data_files=[('/etc/init.d', ['daemon/privaanservice'])],
 
- 
+
     # C'est un système de plugin, mais on s'en sert presque exclusivement
     # Pour créer des commandes, comme "django-admin".
     # Par exemple, si on veut créer la fabuleuse commande "proclame-sm", on
     # va faire pointer ce nom vers la fonction proclamer(). La commande sera
-    # créé automatiquement. 
+    # créé automatiquement.
     # La syntaxe est "nom-de-commande-a-creer = package.module:fonction".
     entry_points = {
         'console_scripts': [
             'privaan = privaan:privaan_run',
         ],
     },
- 
+
     # A fournir uniquement si votre licence n'est pas listée dans "classifiers"
     # ce qui est notre cas
     license="MIT",
- 
+
     # Il y a encore une chiée de paramètres possibles, mais avec ça vous
     # couvrez 90% des besoins
     cmdclass={
         'develop': PostDevelopCommand,
         'install': PostInstallCommand,
     },
- 
+
 )
